26977bab869d533ac54299269f053dc5333f98ac
Michael Chen <mchen0901@gmail.com>
2020-04-11 21:00:03 -0700

    last commit of api

---
+++ client/App.js
@@ -0,0 +1,41 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+import React, { Component } from "react";
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { apiResponse: "" };
+    }
+
+    callAPI() {
+        fetch("http://localhost:9000/testAPI")
+            .then(res => res.text())
+            .then(res => this.setState({ apiResponse: res }))
+            .catch(err => err);
+    }
+
+    componentDidMount() {
+        this.callAPI();
+    }
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text>Welcome to React Native</Text>
+                <Text>With Integration with API</Text>
+                <Text>Data from API: {this.state.apiResponse}</Text>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+});
+
+export default App;
