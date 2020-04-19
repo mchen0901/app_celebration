4b0e21cd6a9c61d232aacd76a8a11f03459c05ee
Anna Leong <awyleong@interchange.ubc.ca>
2020-04-19 12:48:51 -0700

    Adding dynamically populated list as chat box

--- client/components/ChatScreen.js
+++ client/components/ChatScreen.js
@@ -1,35 +1,25 @@
 import 'react-native-gesture-handler';
-import { StyleSheet, Text, View, FlatList, ScrollView, TextInput } from 'react-native';
+import { StyleSheet, Text, View, FlatList, ScrollView, TextInput, TouchableOpacity } from 'react-native';
 import { ThemeProvider, Button } from 'react-native-elements';
 
 import React, { Component } from "react";
-
-// var SampleArray = ["ONE", "TWO"];
 
 export default class ChatScreen extends Component {
 
     constructor(props) {
         super(props);
+        this.array = [{
+      title: 'Enter your messages below!'}],
         this.state = { 
             apiResponse: "", 
             movies: [], 
-            message: ""
+            arrayHolder: [],
+            textInput_Holder: ""
         };
     }
 
-  //   AddItemsToArray=(text)=>{
- 
-  //     //Adding Items To Array.
-  //     SampleArray.push( text );
- 
-  //     // Showing the complete Array on Screen Using Alert.
-  //     Alert.alert(SampleArray.toString());
- 
-  // }
-
     handleMessage = (text) => {
       this.setState({ message: text })
-      // SampleArray.push(text)
    }
 
     callAPI() {
@@ -46,9 +36,28 @@ export default class ChatScreen extends Component {
             .catch(err => err);
     }
 
+
+joinData = () => {
+    this.array.push({title : this.state.textInput_Holder});
+    this.setState({ arrayHolder: [...this.array] })
+  }
+
+  FlatListItemSeparator = () => {
+    return (
+      <View
+        style={{
+          height: 1,
+          width: "100%",
+          backgroundColor: "#607D8B",
+        }}
+      />
+    );
+  }
+
     componentDidMount() {
         this.callAPI();
         this.callMoviesAPI();
+        this.setState({ arrayHolder: [...this.array] })
     }
 
     render() {
@@ -61,8 +70,8 @@ export default class ChatScreen extends Component {
                         onPress={() => this.props.navigation.navigate('Home')}
                     />
                 </ThemeProvider>
-                <View>
-        {/*            <Text>Data from external API: </Text>
+               
+        {/*     <Text>Data from external API: </Text>
         <FlatList
             data={this.state.movies}
             keyExtractor={({ id }, index) => id}
@@ -72,20 +81,37 @@ export default class ChatScreen extends Component {
           />
       */}
 
-        <ScrollView>
-      <Text>Chat Box here! </Text>
-      <Text>{this.state.message}</Text>
-      <TextInput
-        style={{
-          height: 40,
-          borderColor: 'gray',
-          borderWidth: 1
-        }}
-        onChangeText={this.handleMessage}
-        defaultValue="Your message"
-      />
-    </ScrollView>
-      </View>
+
+ <View style={styles.ChatContainer}>
+ <Text style={styles.title}>Chat here</Text>
+        <FlatList
+ 
+          data={this.state.arrayHolder}
+ 
+          width='100%'
+ 
+          extraData={this.state.arrayHolder}
+ 
+          keyExtractor={(index) => index.toString()}
+ 
+          ItemSeparatorComponent={this.FlatListItemSeparator}
+ 
+          renderItem={({ item }) => <Text style={styles.item}> {item.title} </Text>}
+        />
+
+        <TextInput
+          placeholder="Enter Message Here!"
+          onChangeText={data => this.setState({ textInput_Holder: data })}
+          style={styles.textInputStyle}
+          underlineColorAndroid='transparent'
+        />
+ 
+        <TouchableOpacity onPress={this.joinData} activeOpacity={0.7} style={styles.button} >
+ 
+          <Text style={styles.buttonText}> Send </Text>
+ 
+        </TouchableOpacity>
+        </View>
             </View>
         );
     }
@@ -104,4 +130,60 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'center',
     },
+
+      ChatContainer: {
+ 
+    justifyContent: 'center',
+    alignItems: 'center',
+    flex: 1,
+    margin: 5, 
+    borderColor: '#000000', 
+    borderWidth: 2
+ 
+  },
+
+      item: {
+    padding: 10,
+    fontSize: 18,
+    height: 44,
+  },
+ 
+  textInputStyle: {
+ 
+    textAlign: 'center',
+    height: 40,
+    width: '90%',
+    borderWidth: 1,
+    borderColor: '#4CAF50',
+    borderRadius: 7,
+    marginTop: 12
+  },
+ 
+  button: {
+ 
+    width: '90%',
+    height: 40,
+    padding: 10,
+    backgroundColor: '#4CAF50',
+    borderRadius: 8,
+    marginTop: 10
+  },
+ 
+  buttonText: {
+    color: '#fff',
+    textAlign: 'center',
+  },
+
+    title: {
+    marginTop: 16,
+    paddingVertical: 8,
+    borderWidth: 4,
+    borderColor: "#20232a",
+    borderRadius: 6,
+    backgroundColor: "#61dafb",
+    color: "#20232a",
+    textAlign: "center",
+    fontSize: 30,
+    fontWeight: "bold"
+  }
 });
